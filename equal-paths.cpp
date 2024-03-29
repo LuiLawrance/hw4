d03069983d96b4e4b122db3bd0e896ac2d81c1f9
Lawrance Lui <lzlui@usc.edu>
2024-03-29 05:06:28 +0000

    Completed Tree Traversals section / finished equal-paths.cpp

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,30 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool checkEqualPaths(Node *node, int depth, int &leafDepth) {
+    if (node == nullptr) {
+        return true;
+    }
 
-bool equalPaths(Node * root)
-{
-    // Add your code below
+    if (node->left == nullptr && node->right == nullptr) {
+        if (leafDepth == -1) {
+            leafDepth = depth;
+        } else if (depth != leafDepth) {
+            return false;
+        }
+    }
 
+    return checkEqualPaths(node->left, depth + 1, leafDepth) &&
+           checkEqualPaths(node->right, depth + 1, leafDepth);
 }
 
+bool equalPaths(Node *root)
+{
+    // Add your code below
+    if (root == nullptr) {
+        return true;
+    }
+
+    int leafDepth = -1;
+    return checkEqualPaths(root, 0, leafDepth);
+}
